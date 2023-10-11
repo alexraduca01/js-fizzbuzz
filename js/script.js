c3c9942aeb06b0614bddbeda7fc30be665477c4a
alexraduca01 <alex.raduca01@icloud.com>
2023-10-11 16:17:07 +0200

    finished "for" cicle

--- js/script.js
+++ js/script.js
@@ -3,14 +3,27 @@
 // > Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
 for(let a = 1; a <= 100; a++){    
-    
+    let box = document.createElement('div');
+    document.getElementById('wrapper').classList.add('d-flex', 'flex-wrap');
+    document.getElementById('wrapper').append(box);
+    box.classList.add('box', 'd-flex', 'justify-content-center', 'align-items-center');
+
     if((a % 3 === 0 && a % 5 === 0)){
-        console.log('FizzBuzz');
+        box.innerHTML = 'FizzBuzz';
+        box.classList.add('txt-yellow', 'divisible-five-three' , 'fw-bold');
     } else if(a % 3 === 0) {
-        console.log('Fizz');
+        box.innerHTML = 'Fizz';
+        box.classList.add('text-success', 'divisible-three', 'fw-bold');
     } else if(a % 5 === 0){
-        console.log('Buzz');
+        box.innerHTML = 'Buzz';
+        box.classList.add('txt-blue', 'divisible-five' , 'fw-bold');
     } else {
-        console.log(a);
+        box.innerHTML = a;
+        box.classList.add('txt-white');
     }
 }
+
+let credits = document.createElement('p');
+credits.classList.add('credits', 'fs-3', 'text-warning');
+credits.innerHTML = 'Made with &hearts; by alex.';
+document.body.append(credits);

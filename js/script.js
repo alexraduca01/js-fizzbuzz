3cbe3d9b8563ea03e770257b94bbb9eec122edf8
alexraduca01 <alex.raduca01@icloud.com>
2023-10-11 14:52:22 +0200

    created "for" cicle

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@
 // > ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // > Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
-const a;
-const divisibleThree = (a % 3 === 0);
-const divisibleFive = (a % 5 === 0);
-
+for(let a = 1; a <= 100; a++){    
+    
+    if((a % 3 === 0 && a % 5 === 0)){
+        console.log('FizzBuzz');
+    } else if(a % 3 === 0) {
+        console.log('Fizz');
+    } else if(a % 5 === 0){
+        console.log('Buzz');
+    } else {
+        console.log(a);
+    }
+}

8d2bcca3d09b6b9985fd9726e170e6efe8da6ac3
alexraduca01 <alex.raduca01@icloud.com>
2023-10-11 16:55:55 +0200

    moved divs from html to js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,25 @@
 // > ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
 // > Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 
+let sun = document.createElement('div');
+let line1 = document.createElement('div');
+let line2 = document.createElement('div');
+let line3 = document.createElement('div');
+let line4 = document.createElement('div');
+let line5 = document.createElement('div');
+let line6 = document.createElement('div');
+
+document.getElementById('wrapper').append(sun);
+sun.append(line1, line2, line3, line4, line5, line6);
+
+sun.classList.add('sun');
+line1.classList.add('line', 'line1');
+line2.classList.add('line', 'line2');
+line3.classList.add('line', 'line3');
+line4.classList.add('line', 'line4');
+line5.classList.add('line', 'line5');
+line6.classList.add('line', 'line6');
+
 for(let a = 1; a <= 100; a++){    
     let box = document.createElement('div');
     document.getElementById('wrapper').classList.add('d-flex', 'flex-wrap');
